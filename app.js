8fbe51a05ecd3eb44b2a56a4eef37378d12b5ddc
Des Cxcrs <descxcrs@gmail.com>
2019-11-13 11:10:40 -0500

    start protfolio repo

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+//Let Express know we want to use files from "public" directory
+app.use(express.static("public"));
+//Let Express know we want to use .ejs files
+app.set("view engine", "ejs");
+
+//Routes
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.listen(3000, function() {
+  console.log("Server started!");
+});
