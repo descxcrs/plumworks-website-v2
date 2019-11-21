2f0e4295ad5f2862c2ff859bc205c2886aaadc1f
Des Cxcrs <descxcrs@gmail.com>
2019-11-21 10:53:03 -0500

    change page to use fullPage.js

---
+++ public/script/script.js
@@ -0,0 +1,24 @@
+new fullpage("#fullpage", {
+  licenseKey: "FB0E5E4D-55764AD6-AB5CBE48-0642FA07",
+  //   autoScrolling: true,
+  //   keyboardScrolling: true,
+  //   paddingTop: "50px",
+  anchors: ["s1", "s2", "s3"],
+  navigation: true,
+  navigationPosition: "right",
+  navigationTooltips: ["first page", "second page", "third page"]
+
+  //   afterLoad: function(anchorLink, index) {
+  //     console.log("AFTER LOAD - anchorLink:" + anchorLink + " index:" + index);
+  //   },
+  //   onLeave: function(index, nextIndex, direction) {
+  //     console.log(
+  //       "ONLEAVE - index:" +
+  //         index +
+  //         " nextIndex:" +
+  //         nextIndex +
+  //         " direction:" +
+  //         direction
+  //     );
+  //   }
+});
