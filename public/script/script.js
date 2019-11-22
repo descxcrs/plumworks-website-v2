161a6e52b2f3e0aa3feb6608980b29c131695670
Des Cxcrs <descxcrs@gmail.com>
2019-11-22 14:25:26 -0500

    change theme completely.colors.and sections

--- public/script/script.js
+++ public/script/script.js
@@ -1,24 +1,10 @@
-new fullpage("#fullpage", {
+var myFullpage = new fullpage("#fullpage", {
   licenseKey: "FB0E5E4D-55764AD6-AB5CBE48-0642FA07",
   //   autoScrolling: true,
   //   keyboardScrolling: true,
   paddingTop: "2em",
-  anchors: ["s1", "s2", "s3"],
+  anchors: ["about", "projects", "contact"],
   navigation: true,
   navigationPosition: "right",
   navigationTooltips: ["first page", "second page", "third page"]
-
-  //   afterLoad: function(anchorLink, index) {
-  //     console.log("AFTER LOAD - anchorLink:" + anchorLink + " index:" + index);
-  //   },
-  //   onLeave: function(index, nextIndex, direction) {
-  //     console.log(
-  //       "ONLEAVE - index:" +
-  //         index +
-  //         " nextIndex:" +
-  //         nextIndex +
-  //         " direction:" +
-  //         direction
-  //     );
-  //   }
 });
