552b3c60a759e0f38c3f300984c8a7f3024bc674
Des Cxcrs <descxcrs@gmail.com>
2019-11-25 20:38:25 -0500

    enable drag and move

--- public/script/script.js
+++ public/script/script.js
@@ -1,7 +1,6 @@
 var myFullpage = new fullpage("#fullpage", {
   licenseKey: "FB0E5E4D-55764AD6-AB5CBE48-0642FA07",
-  //   autoScrolling: true,
-  //   keyboardScrolling: true,
+  dragAndMove: true,
   paddingTop: "2em",
   anchors: ["about", "projects", "contact"],
   navigation: true,
