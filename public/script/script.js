2e0be8cff901f167123571455a91c9dee90e1479
Desiree <descxcrs@gmail.com>
2020-01-23 11:10:41 -0500

    add scrolloverflow feature

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,7 @@
 var myFullpage = new fullpage("#fullpage", {
   licenseKey: "FB0E5E4D-55764AD6-AB5CBE48-0642FA07",
   dragAndMove: true,
+  scrollOverflow: true,
   paddingTop: "2em",
   anchors: ["about", "projects", "contact"],
   navigation: true,
