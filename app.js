bdaafda3d496a9a25c3d4d2fe56343fa44395092
Desiree <descxcrs@gmail.com>
2020-01-23 13:20:58 -0500

    netlify params

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var sslRedirect = require("heroku-ssl-redirect");
-var express = require("express");
-var app = express();
+const sslRedirect = require("heroku-ssl-redirect");
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+
+const router = express.Router();
 
 //Let Express know we want to use files from "public" directory
 app.use(express.static("public"));
@@ -10,11 +13,17 @@ app.set("view engine", "ejs");
 //enable ssl redirect
 app.use(sslRedirect());
 
+//Netlify router setup - the router needs to know it lives on route "app.js"
+app.use("/.netlift/functions/app", router);
+
 //Routes
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
   res.render("home");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log("Plumworks is ON");
 });
+
+module.exports = app;
+module.exports.handler = serverless(app);
